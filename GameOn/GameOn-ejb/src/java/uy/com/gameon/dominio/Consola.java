b7d6620e716504ea573775ad129dbc1f2f430fd6
Mariana Ferreira <green_girl_1039@hotmail.com>
2015-11-17 17:50:58 -0300

    Se agrega clase consola

---
+++ GameOn/GameOn-ejb/src/java/uy/com/gameon/dominio/Consola.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.com.gameon.dominio;
+
+/**
+ *
+ * @author w8
+ */
+public class Consola {
+    
+}
